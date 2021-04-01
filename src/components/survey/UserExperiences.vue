430a3edade25e32bc7d8d9524866824c0e3e0de7
neerajdask <iamneerajdas@gmail.com>
2021-04-01 16:58:07 +0530

    Fetch data and post data using fetch API.

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,41 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  mounted() {
+    this.loadExperiences();
+    s;
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
   components: {
     SurveyResult,
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        'https://vue-http-demo-3c262-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+
+          this.results = results;
+        });
+    },
   },
 };
 </script>
